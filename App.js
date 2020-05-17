8e7a010a3f9eea43e1578c5a7a9074a7f9d60d9c
Stephenson <salao@debt.com>
2020-05-17 17:33:36 -0400

    buttons

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text Pressed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <Text onPress={handlePress}>Hello JoCD!</Text>
+      <TouchableHighlight onPress={() => console.log("Image Tapped!")}>
+        <Image
+          blurRadius={2}
+          source={{ width: 200, height: 200, uri: "https://picsum.photos/600" }}
+        />
+      </TouchableHighlight> */}
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() => console.log("Button Tapped!")}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
