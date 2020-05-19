14bf56823f311dbd2b74413b1288e1d1805339df
Stephenson <salao@debt.com>
2020-05-19 17:56:21 -0400

    layout

--- App.js
+++ App.js
@@ -1,34 +1,8 @@
 import React from "react";
 import { Text, View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        alignContent: "center",
-        flexWrap: "wrap",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
-    </View>
-  );
+  return <ViewImageScreen />;
 }
---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+
+import colors from "../config/color";
+
+function ViewImageScreen() {
+  return (
+    <View style={styles.cointainer}>
+      <View style={styles.closeIcon}></View>
+      <View style={styles.deleteIcon}></View>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+      />
+    </View>
+  );
+}
+
+export default ViewImageScreen;
+
+const styles = StyleSheet.create({
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.primary,
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+  cointainer: { backgroundColor: colors.black, flex: 1 },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.secondary,
+    position: "absolute",
+    top: 40,
+    right: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
