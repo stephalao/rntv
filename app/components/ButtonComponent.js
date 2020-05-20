d81da7b6a9b3ace83215b6791d6da8f69021c2f2
Stephenson Alao <salao@debt.com>
2020-05-20 17:47:25 -0400

    incomplete button

---
+++ app/components/ButtonComponent.js
@@ -0,0 +1,15 @@
+import { View, StyleSheet } from "react-native";
+
+function ButtonComponent() {
+  <View style={styles.button}></View>;
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "fc5c65",
+  },
+});
+
+export default ButtonComponent;
